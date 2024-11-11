bc40e7f473ec3847c26054bd3fbdca899b33452c
Jacob Kelleran <jacob.kelleran@outlook.com>
2024-11-10 17:19:25 -0800

    Added controls page

--- ui/main_menu.gd
+++ ui/main_menu.gd
@@ -5,7 +5,7 @@ func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://levels/test_scene.tscn")
 
 func _on_controls_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://levels/controls.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
